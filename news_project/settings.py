67b6997bad2487e3c77637eb0a7d4529b87e8064
Ikenna <hnikenna@yahoo.com>
2022-07-16 16:47:39 +0800

    bug_fixed

--- news_project/settings.py
+++ news_project/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.runserver_nostatic',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
