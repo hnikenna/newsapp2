b8125dc49a44c00b7da84b540c0f0d2b1a626670
Ikenna <hnikenna@yahoo.com>
2022-09-13 17:47:34 +0100

    Patch 2.32   fixed mobile autoscroll feature

--- static/js/autoscroll.js
+++ static/js/autoscroll.js
@@ -22,6 +22,12 @@ function Scroller(scrollBtn) {
 for (var i=0; i<scrollBtns.length; i++){
     scrollBtns[i].addEventListener('click', function (e){
         console.log('scope1')
+        if(this.id == 'mobile-scroll'){
+            this.mobile = true
+        } else {
+            this.mobile = false
+
+        }
         if (scrolling === false) {
             console.log('scope2')
             if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
@@ -32,7 +38,13 @@ for (var i=0; i<scrollBtns.length; i++){
                 } else {
                     console.log('scope3')
                     scrolling = true
-                    this.innerHTML = '<b style="color: red;">Stop</b>'
+                    if(this.mobile){
+                        this.innerHTML = '<b style="color: #FFB9B5;">Stop</b>'
+                    } else {
+//                    For Desktop
+                        this.innerHTML = '<b style="color: red;">Stop</b>'
+                    }
+
                     Scroller(this)
                 }
         } else {
