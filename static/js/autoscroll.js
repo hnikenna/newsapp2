fbb7fdccf3eb667d801141ae937ea2b9b6e39ec2
Ikenna <hnikenna@yahoo.com>
2022-09-13 17:30:48 +0100

    Patch 2.31   fixed mobile autoscroll feature

--- static/js/autoscroll.js
+++ static/js/autoscroll.js
@@ -1,17 +1,18 @@
 var scrollBtns = document.getElementsByClassName('scroll-btn')
 var scrolling = false
 
-function Scroller() {
+function Scroller(scrollBtn) {
 //    alerT('', 'Scrolling..')
     if (scrolling === true) {
+
         window.scrollBy(0,10);
-        scrolldelay = setTimeout(Scroller,100);
+        console.log(scrollBtn)
         if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
             // you're at the bottom of the page
-//                console.log('scope5')
+                console.log('scope5')
                 scrolling = false
                 scrollBtn.innerHTML = '<b>Auto-Scroll</b>'
-            }
+            } else {scrolldelay = setTimeout(function(){Scroller(scrollBtn)},100);}
 
     }
 }
@@ -20,21 +21,22 @@ function Scroller() {
 
 for (var i=0; i<scrollBtns.length; i++){
     scrollBtns[i].addEventListener('click', function (e){
-    //    console.log('scope1')
+        console.log('scope1')
         if (scrolling === false) {
-    //        console.log('scope2')
+            console.log('scope2')
             if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
                 // you're at the bottom of the page
-    //                console.log('scope4')
+                    console.log('scope4')
                     scrolling = false
                     this.innerHTML = '<b>Auto-Scroll</b>'
                 } else {
-    //                console.log('scope3')
+                    console.log('scope3')
                     scrolling = true
                     this.innerHTML = '<b style="color: red;">Stop</b>'
-                    Scroller()
+                    Scroller(this)
                 }
         } else {
+            console.log('scope6')
             scrolling = false
             this.innerHTML = '<b>Auto-Scroll</b>'
         }
