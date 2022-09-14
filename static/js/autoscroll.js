e3d500ff9ae844a7619e48c457f7715931fe1792
Ikenna <hnikenna@yahoo.com>
2022-09-14 17:08:48 +0100

    Patch 2.34   Added an alerT on index autoscroll

--- static/js/autoscroll.js
+++ static/js/autoscroll.js
@@ -1,17 +1,25 @@
 var scrollBtns = document.getElementsByClassName('scroll-btn')
 var scrolling = false
-
+var scrollinfo = true
 function Scroller(scrollBtn) {
 //    alerT('', 'Scrolling..')
     if (scrolling === true) {
 
-        if(window.location.pathname === '/') {window.scrollBy(0,475);} else {window.scrollBy(0,2);}
+        if(window.location.pathname === '/') {
+            if(scrollinfo) {
+
+                scrolldelay = setTimeout(function(){alerT('', 'Align article to page center', '#3F1B3C')},50)
+                scrollinfo = false
+            }
+            window.scrollBy(0,475);
+        } else {window.scrollBy(0,2);}
 
 //        console.log(scrollBtn)
         if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
             // you're at the bottom of the page
 //                console.log('scope5')
                 scrolling = false
+                scrollinfo = true
                 scrollBtn.innerHTML = '<b>Auto-Scroll</b>'
             } else {
                 if(window.location.pathname === '/') {
@@ -42,6 +50,7 @@ for (var i=0; i<scrollBtns.length; i++){
                 // you're at the bottom of the page
 //                    console.log('scope4')
                     scrolling = false
+                    scrollinfo = true
                     this.innerHTML = '<b>Auto-Scroll</b>'
                 } else {
 //                    console.log('scope3')
@@ -58,6 +67,7 @@ for (var i=0; i<scrollBtns.length; i++){
         } else {
 //            console.log('scope6')
             scrolling = false
+            scrollinfo = true
             this.innerHTML = '<b>Auto-Scroll</b>'
         }
     })
