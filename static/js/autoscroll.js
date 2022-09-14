8ca7a159cbbfeaa1b8e7ae7f8295ba1fe0672dc6
Ikenna <hnikenna@yahoo.com>
2022-09-14 16:58:55 +0100

    Patch 2.34   Redefined the autoscroll

--- static/js/autoscroll.js
+++ static/js/autoscroll.js
@@ -5,14 +5,22 @@ function Scroller(scrollBtn) {
 //    alerT('', 'Scrolling..')
     if (scrolling === true) {
 
-        window.scrollBy(0,10);
-        console.log(scrollBtn)
+        if(window.location.pathname === '/') {window.scrollBy(0,475);} else {window.scrollBy(0,2);}
+
+//        console.log(scrollBtn)
         if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
             // you're at the bottom of the page
-                console.log('scope5')
+//                console.log('scope5')
                 scrolling = false
                 scrollBtn.innerHTML = '<b>Auto-Scroll</b>'
-            } else {scrolldelay = setTimeout(function(){Scroller(scrollBtn)},100);}
+            } else {
+                if(window.location.pathname === '/') {
+                    scrolldelay = setTimeout(function(){Scroller(scrollBtn)},3000);
+                    } else {
+                        scrolldelay = setTimeout(function(){Scroller(scrollBtn)},50);
+                    }
+
+            }
 
     }
 }
@@ -21,7 +29,7 @@ function Scroller(scrollBtn) {
 
 for (var i=0; i<scrollBtns.length; i++){
     scrollBtns[i].addEventListener('click', function (e){
-        console.log('scope1')
+//        console.log('scope1')
         if(this.id == 'mobile-scroll'){
             this.mobile = true
         } else {
@@ -29,14 +37,14 @@ for (var i=0; i<scrollBtns.length; i++){
 
         }
         if (scrolling === false) {
-            console.log('scope2')
+//            console.log('scope2')
             if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
                 // you're at the bottom of the page
-                    console.log('scope4')
+//                    console.log('scope4')
                     scrolling = false
                     this.innerHTML = '<b>Auto-Scroll</b>'
                 } else {
-                    console.log('scope3')
+//                    console.log('scope3')
                     scrolling = true
                     if(this.mobile){
                         this.innerHTML = '<b style="color: #DFFF40;">Stop</b>'
@@ -48,7 +56,7 @@ for (var i=0; i<scrollBtns.length; i++){
                     Scroller(this)
                 }
         } else {
-            console.log('scope6')
+//            console.log('scope6')
             scrolling = false
             this.innerHTML = '<b>Auto-Scroll</b>'
         }
